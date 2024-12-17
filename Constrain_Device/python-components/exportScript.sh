fe0e1d4f3b3d17dbae213e2b75689d00e72a2f9b
Vishweshkota <kotavishwesh01@gmail.com>
2024-12-17 15:06:55 -0500

    Gateway initial commit

--- Constrain_Device/python-components/exportScript.sh
+++
@@ -1,3 +0,0 @@
-echo "Exporting Python Path"
-export PYTHONPATH=/home/vishwesh/connectedDevices/cda/python-components/src/main/python/:/home/vishwesh/connectedDevices/cda/python-components/src/test/python
-echo $PYTHONPATH
